2e79efccf3a1b714f3d9743178a83a65579177d0
iisquare <iisquare@163.com>
2019-01-17 17:36:13 +0800

    test

--- test/promise.js
+++ test/promise.js
@@ -1,18 +1,26 @@
 
-new Promise((r, d) => {
-	setTimeout(() => {
-		console.log('12214')
-		r(2222)
-	}, 20)
-}).then(r => {
+async function fn() {
+	return new Promise((r, d) => {
+		setTimeout(() => {
+			console.log('12214')
+			r(2222)
+		}, 20)
+	}).then(r => {
+		console.log(r)
+		return Promise.reject(44444)
+	}).then(r => {
+		console.log(r)
+		return Promise.resolve(555555555)
+	}).catch(e => {
+		console.log(e)
+		return Promise.resolve(33333333)
+	}).then(r => {
+		console.log(r)
+		return Promise.resolve(0000)
+	})
+}
+
+(async function () {
+	let r = await fn()
 	console.log(r)
-	return Promise.resolve(44444)
-}).then(r => {
-  console.log(r)
-  return Promise.resolve(555555555)
-}).catch(e => {
-  console.log(e)
-  return Promise.resolve(33333333)
-}).then(r => {
-  console.log(r)
-})
+})()
