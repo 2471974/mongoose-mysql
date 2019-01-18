62e126d941fdc645687b4375e137a254e25c497f
iisquare <iisquare@163.com>
2019-01-18 11:23:14 +0800

    test

--- test/mongoose.js
+++ test/mongoose.js
@@ -18,5 +18,6 @@ kitty.save(function (err, doc) {
   }
   console.log(doc._id, doc._id.toHexString(), doc._id.toString())
   Cat.findById(doc._id).then(r => console.log(r))
-  Cat.findById(ObjectId('xxxxxxxxxxxx')).then(r => console.log('>>>>>', r)).catch(e => console.log(e))
+  Cat.findById({_id: '5c4144edbb7896896e214174'}).then(r => console.log('>>>>>', r)).catch(e => console.log(e))
+  Cat.findById({_id: 'xxxxxxxxxxxx'}).then(r => console.log('>>>>>', r)).catch(e => console.log(e))
 });
